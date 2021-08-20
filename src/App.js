f95056a284d3fc0d6df198dbc1bf3013ac1e215a
Aleksandra <stormofe@gmail.com>
2021-08-20 14:53:24 +0300

    Use randomecolor, uuid, draggable. Write 50%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import {v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable'
 
 function App() {
+
+	const [item, setItem] = useState('');
+	const [items, setItems] = useState(
+		JSON.parse(localStorage.getItem('items')) || []
+	)
+
+	useEffect(() => {
+		localStorage.setItem('items', JSON.stringify(items))
+	}, [items])
+
+	const newItem = () => {
+		if(item.trim() !== '') {
+			const newItem = {
+				id: uuidv4(),
+				item, 
+				color: randomColor({
+					luminosity: 'light'
+				}),
+				defaultPos: {
+					x: 300,
+					y: -300
+				}
+			}
+			setItems((items) => [...items, newItem]);
+			setItem('');
+
+		} else {
+			alert('Add something')
+			setItem('');
+
+		}
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	  <div className="App">
+		  <div className="wrapper">
+			  <input 
+			  type="text" 
+			  placeholder="Add new task"
+			  onChange={(e) => setItem(e.target.value)}/>
+			  <button className="enter" onClick={newItem}>Add</button>
+		  </div>
+		  {
+			  items.map((item, index) => {
+				return (
+					<Draggable key={index} defaultPosition={item.defaultPos}>
+						<div className="todo__item" style={{backgroundColor: item.color}}>
+							{`${item}`}
+							<button className="delete">X</button>
+						</div>
+					</Draggable>
+				)
+			  })
+		  }
+	  </div>
+  )
 }
 
 export default App;
