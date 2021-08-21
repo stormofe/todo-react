c004725b175ded8d540c926ab75b033181eacc65
Aleksandra <stormofe@gmail.com>
2021-08-21 12:08:06 +0300

    Finish todo-list. Add press Enter, save position in localstorage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import {v4 as uuidv4 } from 'uuid';
-import { randomColor } from 'randomcolor';
+import  randomColor  from 'randomcolor';
 import Draggable from 'react-draggable'
 
 function App() {
@@ -37,23 +37,47 @@ function App() {
 
 		}
 	}
+	const deleteNode = (id) => {
+		setItems(items.filter(item => item.id !== id));
+	}
+	const updatePos = (data, index) => {
+		let newArray = [...items];
+		newArray[index].defaultPos = {x: data.x, y: data.y};
+		setItems(newArray)
+	}
+
+	const keyPress = (e) => {
+		const code = e.keyCode || e.which;
+		if (code === 13) {
+			newItem();
+		}
+	}
 
   return (
 	  <div className="App">
 		  <div className="wrapper">
 			  <input 
+			  value={item}
 			  type="text" 
 			  placeholder="Add new task"
-			  onChange={(e) => setItem(e.target.value)}/>
+			  onChange={(e) => setItem(e.target.value)}
+			  onKeyPress={e => keyPress(e)}/>
 			  <button className="enter" onClick={newItem}>Add</button>
 		  </div>
 		  {
+			  
 			  items.map((item, index) => {
 				return (
-					<Draggable key={index} defaultPosition={item.defaultPos}>
+					<Draggable key={index} defaultPosition={item.defaultPos} onStop={(e, data) => {
+						updatePos(data, index)
+					}}>
+						
 						<div className="todo__item" style={{backgroundColor: item.color}}>
-							{`${item}`}
-							<button className="delete">X</button>
+							
+							{`${item.item}`}
+							<button 
+							className="delete"
+							onClick={() => deleteNode(item.id)}>X</button>
 						</div>
 					</Draggable>
 				)
